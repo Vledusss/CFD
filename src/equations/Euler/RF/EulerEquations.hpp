a269f2aff0e6acc22139c9c11f42b99bf5091ad4
Vlad Chugunov <vladislav.chugunov@yandex.ru>
2025-12-09 03:39:53 +0300

    removed euler equations constructor

--- src/equations/Euler/RF/EulerEquations.hpp
+++ src/equations/Euler/RF/EulerEquations.hpp
@@ -13,10 +13,6 @@ template<indexType N>
 struct Equation {
     std::array<State, N> states;
     
-    Equation(const double gamma = 1.4) {
-        for (auto& state : states) { state = State(0, 0, 0, gamma); }
-    }
-    
     Flux calcFlux(const State& state) const {
         const double p = state.getPressure();
         const double u = state.getVelocity();
